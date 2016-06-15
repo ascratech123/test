d7e6afefe9bdd1a44bcdc48e0318c5c5f7d2d06e
shiv@ascratech.com <shiv@ascratech.com>
2016-06-15 14:05:22 +0530

    notification read unread

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -110,7 +110,7 @@ class Notification < ActiveRecord::Base
   end
 
   def push_to_ios(token, notification, push_pem_file, ios_obj, b_count, msg, push_page, type, time)
-    notification = Grocer::Notification.new("device_token" => token, "alert"=>{"title"=> push_pem_file.title, "body"=> msg, "action"=> "Read"}, 'content_available' => true, "badge" => b_count, "sound" => "siren.aiff", "custom" => {"push_page" => push_page, "id" => '1', 'event_id' => notification.event_id, 'image_url' => notification.image.url, 'type' => type, 'created_at' => time})
+    notification = Grocer::Notification.new("device_token" => token, "alert"=>{"title"=> push_pem_file.title, "body"=> msg, "action"=> "Read"}, 'content_available' => true, "badge" => b_count, "sound" => "siren.aiff", "custom" => {"push_page" => push_page, "id" => '1', 'event_id' => notification.event_id, 'image_url' => notification.image.url, 'type' => type, 'created_at' => time, 'notification_id' => notification.id})
     response = ios_obj.push(notification)
     Rails.logger.info("******************************#{response}****************************************************")
   end
