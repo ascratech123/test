86d19763b3093aaaf1904ff659266494556e1ed0
shiv@ascratech.com <shiv@ascratech.com>
2016-06-17 13:41:09 +0530

    notification unread count

--- app/models/invitee_notification.rb
+++ app/models/invitee_notification.rb
@@ -1,3 +1,5 @@
 class InviteeNotification < ActiveRecord::Base
 	belongs_to :notification
+
+  validates :invitee_id, :notification_id, :event_id, presence: { :message => "This field is required." }
 end
