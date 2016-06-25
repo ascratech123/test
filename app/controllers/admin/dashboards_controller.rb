9b4e595305bc1e9d97bdaa068c3445be17ebbd1f
Sushil Deothakle <sushil@ascratech.in>
2016-06-25 18:47:32 +0530

    telecaller new functionality

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -5,6 +5,8 @@ class Admin::DashboardsController < ApplicationController
   def index
     if current_user.has_role? :moderator
       @count = Event.with_role(:moderator, current_user)
+    elsif current_user.has_role? :telecaller
+      @count = Event.with_role(:telecaller, current_user)
     end
     if current_user.has_role? :event_admin
       @count = Event.with_roles(current_user.roles.pluck(:name), current_user)
