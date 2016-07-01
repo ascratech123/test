6e6ff3a9b3191af4738f43fe18f3c10ab9c693e1
Sushil Deothakle <sushil@ascratech.in>
2016-07-01 17:01:54 +0530

    user registration validation changes

--- app/models/user_registration.rb
+++ app/models/user_registration.rb
@@ -9,19 +9,35 @@ class UserRegistration < ActiveRecord::Base
         attre.each do |regi_valid|
           self_attre = self.attributes.except("id", "registration_id","invitee_id", "event_id")
           self_attre.each do |user_regi_valid|
+            if (regi_valid[1][:validation_type].present? and regi_valid[1][:mandatory_field].present?)
+              self.validation_and_mandate_present(regi_valid,user_regi_valid)
+            end
             if regi_valid[1][:validation_type].present?
-              if regi_valid[1][:validation_type] == "Mandatory"
-                errors.add(user_regi_valid[0], "This field is required.") if regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].blank?
-              elsif regi_valid[1][:validation_type] == "Email Validation"
-                errors.add(user_regi_valid[0], "Sorry, this doesn't look like a valid email.") if regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].present? and user_regi_valid[1].match(/\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i) == nil
-              elsif regi_valid[1][:validation_type] == "Numeric only"
-                errors.add(user_regi_valid[0], "This field required only Numeric. ") if (regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].present? and (user_regi_valid[1] =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/ ? false : true))
-              end      
-            else
-              errors.add(user_regi_valid[0], "This field is required.") if((regi_valid[1][:mandatory_field].present? and regi_valid[1][:mandatory_field] == "yes") and regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].blank?)
+              self.validation_present(regi_valid,user_regi_valid)
+            end
+            if (regi_valid[1][:validation_type].blank? and regi_valid[1][:mandatory_field].present?)
+              self.mandate_present(regi_valid,user_regi_valid)
             end
           end
         end
       end
+    end
+
+    def validation_and_mandate_present(regi_valid,user_regi_valid)
+      errors.add(user_regi_valid[0], "This field is required.") if((regi_valid[1][:mandatory_field].present? and regi_valid[1][:mandatory_field] == "yes") and regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].blank?)
+    end
+
+    def validation_present(regi_valid,user_regi_valid)
+      if regi_valid[1][:validation_type] == "Mandatory"
+        errors.add(user_regi_valid[0], "This field is required.") if regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].blank?
+      elsif regi_valid[1][:validation_type] == "Email Validation"
+        errors.add(user_regi_valid[0], "Sorry, this doesn't look like a valid email.") if regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].present? and user_regi_valid[1].match(/\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i) == nil
+      elsif regi_valid[1][:validation_type] == "Numeric only"
+        errors.add(user_regi_valid[0], "This field required only Numeric. ") if (regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].present? and (user_regi_valid[1] =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/ ? false : true))
+      end
+    end
+
+    def mandate_present(regi_valid,user_regi_valid)
+      errors.add(user_regi_valid[0], "This field is required.") if((regi_valid[1][:mandatory_field].present? and regi_valid[1][:mandatory_field] == "yes") and regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].blank?)
     end
 end
