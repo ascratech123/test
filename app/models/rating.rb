959ecb6ebb64a69a5688a0db013b17405e625b60
shivkmr94 <shivkmr94@gmail.com>
2016-07-25 13:24:13 +0530

    analytics issue

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -65,8 +65,8 @@ class Rating < ActiveRecord::Base
   end
 
   def self.get_top_three_speaker_ids(event_id, start_date=nil, to_date=nil)
-    from_date = from_date.blank? ? Date.today : from_date.to_datetime
-    to_date = to_date.blank? ? Date.today : to_date.to_datetime
+    start_date = start_date.blank? ? Date.today : start_date.to_date
+    to_date = to_date.blank? ? Date.today : to_date.to_date
     speaker_ids = Speaker.where(:event_id => event_id).pluck(:id)
     # arr = Rating.where('ratable_type = ? and ratable_id IN (?) and Date(created_at) >= ? and Date(created_at) <= ?', 'Speaker', speaker_ids, start_date, to_date).group(:ratable_id).count.sort_by{|k, v| v}.last(3).map{|a| a[0]}.compact
     ratings = Rating.where('ratable_type = ? and ratable_id IN (?)', 'Speaker', speaker_ids).group_by(&:ratable_id)#.count.sort_by{|k, v| v}.last(3).map{|a| a[0]}.compact
@@ -75,10 +75,10 @@ class Rating < ActiveRecord::Base
   end
 
   def self.get_top_rated(return_count, event_id, type, start_date, to_date)
-    from_date = from_date.blank? ? Date.today : from_date.to_datetime
-    to_date = to_date.blank? ? Date.today : to_date.to_datetime
+    start_date = start_date.blank? ? Date.today : start_date.to_date
+    to_date = to_date.blank? ? Date.today : to_date.to_date
     ratable_ids = Speaker.where(:event_id => event_id).pluck(:id) if type == 'Speaker'
-    ratable_ids = Agenda.where(:event_id => event_id).pluck(:id) if type == 'Session'
+    ratable_ids = Agenda.where(:event_id => event_id).pluck(:id) if type == 'Agenda'
     ratings = Rating.where('ratable_type = ? and ratable_id IN (?) and Date(created_at) >= ? and Date(created_at) <= ?', type, ratable_ids, start_date, to_date).group_by(&:ratable_id)#.count.sort_by{|k, v| v}.last(3).map{|a| a[0]}.compact
     arr = ratings.map{|k, v| [k, v.map{|a| a.rating}.sum / v.count]}.sort_by{|k| k[1]}.last(return_count)
     arr.reverse
