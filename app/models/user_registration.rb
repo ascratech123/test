1b9e636b20aea80a39d2dd25fbd2e8cb16e5324a
shivkmr94 <shivkmr94@gmail.com>
2016-07-26 16:07:50 +0530

    registered user reject and appove

--- app/models/user_registration.rb
+++ app/models/user_registration.rb
@@ -1,25 +1,45 @@
 class UserRegistration < ActiveRecord::Base
-    before_validation :check_validation
+  before_validation :check_validation
 
-    def check_validation
-      event = Event.find(self.event_id)
-      registration = event.registrations
-      registration.each do |validation|
-        attre = validation.attributes.except("id", "event_id","created_at", "updated_at","custom_css","custom_js","custom_source_code", 'status')
-        attre.each do |regi_valid|
-          self_attre = self.attributes.except("id", "registration_id","invitee_id", "event_id")
-          self_attre.each do |user_regi_valid|
-            if regi_valid[1].present? and regi_valid[1][:validation_type].present?
-              if regi_valid[1].present? and regi_valid[1][:validation_type] == "Mandatory"
-                errors.add(user_regi_valid[0], "This field is required.") if regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].blank?
-              elsif regi_valid[1].present? and regi_valid[1][:validation_type] == "Email Validation"
-                errors.add(user_regi_valid[0], "Sorry, this doesn't look like a valid email.") if regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].present? and user_regi_valid[1].match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i) == nil
-              elsif regi_valid[1].present? and regi_valid[1][:validation_type] == "Numeric only"
-                errors.add(user_regi_valid[0], "This field required only Numeric. ") if (regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].present? and (user_regi_valid[1] =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/ ? false : true))
-              end      
-            end
+
+  include AASM
+  aasm :column => :status do
+    state :pending, :initial => true
+    state :approved
+    state :rejected
+    
+    event :approve do
+      transitions :from => [:pending, :rejected], :to => [:approved]
+    end 
+    event :reject do
+      transitions :from => [:pending,:approved], :to => [:rejected]
+    end
+  end 
+
+  def check_validation
+    event = Event.find(self.event_id)
+    registration = event.registrations
+    registration.each do |validation|
+      attre = validation.attributes.except("id", "event_id","created_at", "updated_at","custom_css","custom_js","custom_source_code", 'status')
+      attre.each do |regi_valid|
+        self_attre = self.attributes.except("id", "registration_id","invitee_id", "event_id")
+        self_attre.each do |user_regi_valid|
+          if regi_valid[1].present? and regi_valid[1][:validation_type].present?
+            if regi_valid[1].present? and regi_valid[1][:validation_type] == "Mandatory"
+              errors.add(user_regi_valid[0], "This field is required.") if regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].blank?
+            elsif regi_valid[1].present? and regi_valid[1][:validation_type] == "Email Validation"
+              errors.add(user_regi_valid[0], "Sorry, this doesn't look like a valid email.") if regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].present? and user_regi_valid[1].match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i) == nil
+            elsif regi_valid[1].present? and regi_valid[1][:validation_type] == "Numeric only"
+              errors.add(user_regi_valid[0], "This field required only Numeric. ") if (regi_valid[0] == user_regi_valid[0] and user_regi_valid[1].present? and (user_regi_valid[1] =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/ ? false : true))
+            end      
           end
         end
       end
     end
+  end
+
+  def perform_event(event)
+    self.approve! if event == 'approve'
+    self.reject! if event == 'reject'
+  end
 end
