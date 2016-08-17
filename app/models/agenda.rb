c2ddd66f7f2d704ba6ede1614724047a9747f81b
Ubuntu <ubuntu@ip-172-31-19-15.ap-southeast-1.compute.internal>
2016-08-17 16:31:50 +0530

    agenda to time

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -55,8 +55,8 @@ class Agenda < ActiveRecord::Base
     end_date = self.end_agenda_date rescue nil
     start_time = "#{start_date.strftime('%d/%m/%Y')} #{self.start_time_hour.gsub(':', "") rescue nil}:#{self.start_time_minute.gsub(':', "")  rescue nil}:#{0} #{self.start_time_am}" if start_date.present?
     end_time = "#{end_date.strftime('%d/%m/%Y')} #{self.end_time_hour.gsub(':', "")  rescue nil}:#{self.end_time_minute.gsub(':', "")  rescue nil}:#{0} #{self.end_time_am}" if end_date.present?
-    self.start_agenda_time = start_time.to_datetime if start_date.present?
-    self.end_agenda_time = end_time.to_datetime if end_date.present?
+    self.start_agenda_time = start_time.to_time if start_date.present?
+    self.end_agenda_time = end_time.to_time if end_date.present?
   end
 
   def set_end_date_if_end_date_not_selected
