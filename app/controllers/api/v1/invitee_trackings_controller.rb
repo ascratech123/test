0e25d43d3df8a776f4019cb07a03159fb2e012a6
Sushil Deothakle <sushil@ascratech.in>
2016-08-17 11:50:06 +0530

    access control origin issue qr code

--- app/controllers/api/v1/invitee_trackings_controller.rb
+++ app/controllers/api/v1/invitee_trackings_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::InviteeTrackingsController < ApplicationController
   skip_before_action :load_filter
   skip_before_action :authenticate_user!
-  before_action :qr_code_access , :only => [:create]
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
+
   respond_to :json
   def index
     event = Event.find_by_id(params[:event_id])
@@ -23,4 +25,22 @@ class Api::V1::InviteeTrackingsController < ApplicationController
       render :status=>200, :json=>{:status=>"Failure",:message=>"Event Not Found."}
     end
   end
+
+  protected
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end  
 end
