e6c1da7cdccfcc2f67324f573b250d7a094bc491
Sushil Deothakle <sushil@ascratech.in>
2016-08-19 17:05:00 +0530

    access control error add PUT the metohod present in protected

--- app/controllers/api/v1/invitee_trackings_controller.rb
+++ app/controllers/api/v1/invitee_trackings_controller.rb
@@ -46,14 +46,14 @@ class Api::V1::InviteeTrackingsController < ApplicationController
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PATCH, PUT, OPTIONS'
     headers['Access-Control-Max-Age'] = "1728000"
   end
 
   def cors_preflight_check
     if request.method == :options
       headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PATCH, PUT, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
       headers['Access-Control-Max-Age'] = '1728000'
       render :text => '', :content_type => 'text/plain'
