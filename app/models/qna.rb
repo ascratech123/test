42822b3c4ba01f15198f9c2fd5c43486fa73a9bc
uday <udmagdum@gmail.com>
2016-08-30 12:31:47 +0530

    query optimization

--- app/models/qna.rb
+++ app/models/qna.rb
@@ -43,8 +43,8 @@ class Qna < ActiveRecord::Base
   end
 
   def get_panel_name
-    @panel_name = Panel.find_by_id(self.receiver_id)
-    @panel_name.present? ? @panel_name.name : "" 
+    @panel = Panel.find_by_id(self.receiver_id)
+    @panel.present? ? @panel.name : "" 
   end 
 
   def get_user_name
