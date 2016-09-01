42eb80aae9429c6f0d3df679c473a6b802bceaa7
uday <uday@ascratech.com>
2016-09-01 07:17:21 +0000

    Revert "scaned invitees export"
    This reverts commit 89a42b31468fc89b265ac750af4c5923e8bdb87c

--- app/controllers/admin/invitees_controller.rb
+++ app/controllers/admin/invitees_controller.rb
@@ -1,9 +1,9 @@
 class Admin::InviteesController < ApplicationController
   layout 'admin'
 
-  load_and_authorize_resource
+  #load_and_authorize_resource
+  before_filter :check_user_role, :except => [:index]
   before_filter :authenticate_user, :authorize_event_role, :find_features
-
   
 
   def index
@@ -95,6 +95,11 @@ class Admin::InviteesController < ApplicationController
 
   protected
 
+  def check_user_role
+    if (!current_user.has_role? :db_manager) and (!current_user.has_role? :licensee_admin)
+      redirect_to admin_dashboards_path
+    end  
+  end
   def invitee_params
     params.require(:invitee).permit!
   end
