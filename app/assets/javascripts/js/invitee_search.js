ca2bc157ce1224f407f16fb0b17114ae31098479
Sushil Deothakle <sushil@ascratech.in>
2016-09-02 14:43:17 +0530

    invitee search new section

---
+++ app/assets/javascripts/js/invitee_search.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('ul.tabs li').click(function(){
+    var tab_id = $(this).attr('data-tab');
+
+    $('ul.tabs li').removeClass('current');
+    $('.tab-content').removeClass('current');
+
+    $(this).addClass('current');
+    $("#"+tab_id).addClass('current');
+  });
+});
+  
+
+$(document).ready(function(){
+  if ($("#params_value").attr("params_value") == "attendee"){
+    $("#attendee").trigger("click");
+  }
+});
+
+$(document).ready(function(){
+  if ($("#params_value").attr("params_value") == "printBadge"){
+    $("#printBadge").trigger("click");
+  }
+});
