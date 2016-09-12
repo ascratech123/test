855b4e442617f0d209a083e6b8a257fbaa4295f9
uday <udmagdum@gmail.com>
2016-09-12 12:26:23 +0530

    controller changes

--- app/controllers/admin/qr_scanner_details_controller.rb
+++ app/controllers/admin/qr_scanner_details_controller.rb
@@ -5,8 +5,8 @@ class Admin::QrScannerDetailsController < ApplicationController
 
 
   def index
-    @event = Event.where(:id=>params[:event_id])
-    @badge_pdf = @event.first.badge_Pdf
+  	@event = Event.where(:id=>params[:event_id])
+    @badge_pdf = @event.first.badge_pdf#.find_by_event_id(params[:event_id])
     @attendees = Invitee.where(:qr_code_registration => true, :event_id => params[:event_id]).order('updated_at desc')
   end
 
