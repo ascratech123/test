15971b829899b538cf7189c0c5bd80dd894cb8b4
pratik <pratikchauhan.214@gmail.com>
2016-09-14 16:17:14 +0530

    first and last name added & full name remove from manage_invitee_field

--- app/models/manage_invitee_field.rb
+++ app/models/manage_invitee_field.rb
@@ -2,7 +2,7 @@ class ManageInviteeField < ActiveRecord::Base
   serialize :field_attr, Hash
   belongs_to :event
 
-  DISPLAY_FIELD_NAME = {"name_of_the_invitee"=>"Full Name", "email"=>"Email", "company_name"=>"Company Name", "designation"=>"Designation"}
+  DISPLAY_FIELD_NAME = {"first_name"=>"First Name", "last_name"=>"Last Name", "email"=>"Email", "company_name"=>"Company Name", "designation"=>"Designation"}
 
   def get_column_names
     hsh = DISPLAY_FIELD_NAME
