ed059555dccc28fa33a0c4ecdc9fac111d833cff
uday <udmagdum@gmail.com>
2016-09-19 15:26:07 +0530

    timezone api issues

--- app/controllers/admin/time_zones_controller.rb
+++ app/controllers/admin/time_zones_controller.rb
@@ -17,9 +17,19 @@ class Admin::TimeZonesController < ApplicationController
 	  		lng = position["results"][0]["geometry"]["location"]["lng"]
 	  		timezone_detail = TimeZoneApi.get_time_zone(lat,lng)
 	  		@timezone = timezone_detail["timeZoneId"].split("/").second.titleize if timezone_detail.present?
-	  		if @timezone == "Calcutta"
-	  			@timezone = "Kolkata"
-	  		end	
+	  		r_timezone = ActiveSupport::TimeZone.all.sort_by(&:name).map{|e| e.name}
+	  		
+	  		if r_timezone.include?(ActiveSupport::TimeZone[timezone_detail["rawOffset"]].name)
+	  			@timezone = ActiveSupport::TimeZone[timezone_detail["rawOffset"]].name
+				elsif r_timezone.include?(@timezone)
+	  			@timezone
+				end
+
+				signle_timezone = ActiveSupport::TimeZone.all.sort_by(&:name).map{|e| ["GMT#{e.formatted_offset}", e.name]}
+				timezone_data = signle_timezone.select{ |a| a[1] == @timezone}
+				if timezone_data.present?
+					@timezone = timezone_data.first[0]
+				end		
 			end		
 		end	
   end
