f6e322d00f8d81649e297cb8225167c79314fa8c
Gayatri Vijaykumar <gayatri@ascratech.com>
2016-09-19 19:21:13 +0530

    timezone changes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ namespace :admin do
   get 'bee_editor/token' => 'bee_editors#token'
   get 'bee_editor/template' => 'bee_editors#template'
   # get '/check_email_existance' => 'users#check_email_existance'
+  resources :time_zones
   resources :dashboards, :themes, :manage_users, :users, :roles, :homes, :smtp_settings
   resources :profiles, :manage_mobile_apps, :downloads, :external_login
   resources :licensees do
