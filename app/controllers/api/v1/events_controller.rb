3f751f1b423251de3c62bdb0cf216b4cc7ee434c
Gayatri Vijaykumar <gayatri@ascratech.com>
2016-09-19 03:37:01 -0400

    api changes for replacing preview_code with mobile_application_code

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -5,8 +5,9 @@ class Api::V1::EventsController < ApplicationController
   before_filter :check_date, :only => :index
 
   def index
-    mobile_application = MobileApplication.find_by_submitted_code(params[:mobile_application_code]) || MobileApplication.find_by_preview_code(params[:mobile_application_preview_code])
-    submitted_app = "Yes" if params[:mobile_application_code].present?
+    mobile_application = MobileApplication.where('submitted_code =? or preview_code =?', params[:mobile_application_code], params[:mobile_application_code]).first
+    # mobile_application = MobileApplication.find_by_submitted_code(params[:mobile_application_code]) || MobileApplication.find_by_preview_code(params[:mobile_application_preview_code])
+    submitted_app = "Yes" if params[:mobile_application_code].present? and mobile_application.submitted_code == params[:mobile_application_code]
     if mobile_application.present?
 #======mine
 #      sync_time = Time.now.to_s
