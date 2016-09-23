f91d192270ae95c6a61137f29262ceea22e57b92
uday <uday@ascratech.com>
2016-09-23 11:40:03 +0000

    Update time_zone_api.rb

--- lib/tasks/time_zone_api.rb
+++ lib/tasks/time_zone_api.rb
@@ -6,7 +6,7 @@ class TimeZoneApi
 
 	def self.get_lat_long_from_city(city_name,country_name)
 		address = "#{country_name}" "+" "#{city_name}"
-		HTTParty.get "http://maps.googleapis.com/maps/api/geocode/json?address=#{address}"
+		HTTParty.get "http://maps.googleapis.com/maps/api/geocode/json?address=city:#{city_name},country:#{country_name}"
 	end
 
 	def self.get_time_zone(lat,lng,timestamp)
