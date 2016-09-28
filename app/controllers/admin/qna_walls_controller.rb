94c0b7ac49108408e5e93172eafe3c135650a469
uday <udmagdum@gmail.com>
2016-09-28 12:35:53 +0530

    controller changes

---
+++ app/controllers/admin/qna_walls_controller.rb
@@ -0,0 +1,42 @@
+class Admin::QnaWallsController < ApplicationController
+  
+  layout 'admin'
+  before_filter :authenticate_user, :find_event
+  
+	def new
+		@qna_wall = QnaWall.new
+	end
+	
+  def create
+		@qna_wall = QnaWall.new(qna_wall_params)
+    if @qna_wall.save
+      redirect_to edit_admin_event_qna_wall_path(:event_id => @qna_wall.event_id,:id =>@qna_wall.id)
+    else
+      render :action => 'new'
+    end
+	end
+
+	def edit
+  	@qna_wall = QnaWall.find(params[:id])
+  end
+  
+  def update
+    @qna_wall = QnaWall.find(params[:id])
+    if @qna_wall.update_attributes(qna_wall_params)
+      redirect_to edit_admin_event_qna_wall_path(:event_id => @qna_wall.event_id,:id =>@qna_wall.id)
+    else
+      render :action => "edit"
+    end
+  end
+	
+  protected
+	
+  def find_event
+		@event = Event.find(params[:event_id])
+	end	
+	
+	def qna_wall_params
+    params.require(:qna_wall).permit!
+  end
+
+end
