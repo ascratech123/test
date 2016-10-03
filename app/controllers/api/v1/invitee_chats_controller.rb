21764757a2d6a52f3651729075dd0e284d3968e1
uday <udmagdum@gmail.com>
2016-10-03 20:46:30 +0530

    sorting chats

--- app/controllers/api/v1/invitee_chats_controller.rb
+++ app/controllers/api/v1/invitee_chats_controller.rb
@@ -12,18 +12,20 @@ class Api::V1::InviteeChatsController < ApplicationController
 				@invitees_list = @recieve_chats_invitee_ids + @send_chats_invitee_ids
 				
 				#@invitees_list = @chats.pluck(:sender_id,:member_ids).flatten.map { |x| x.to_i}.uniq
-				@invitees = Invitee.where("id IN (?)",@invitees_list)
 				invitees = []
-				@invitees.each do |invitee|
-					data = {}
-					data["id"] = invitee.id
-					data["first_name"] = invitee.first_name
-					data["last_name"] = invitee.last_name
-					data["profile_picture"] = invitee.profile_picture
-					data["unread_chat_count"]= invitee.unread_chat_count(params[:invitee_id])
-					data["created_at_with_event_timezone"] = invitee.created_at_with_event_timezone 
-					data["updated_at_with_event_timezone"] = invitee.updated_at_with_event_timezone
-					invitees << data
+				@invitees_list.each do |invitee|
+					@invitee = Invitee.find(invitee)
+					if @invitee.present?
+						data = {}
+						data["id"] = @invitee.id
+						data["first_name"] = @invitee.first_name
+						data["last_name"] = @invitee.last_name
+						data["profile_picture"] = @invitee.profile_picture
+						data["unread_chat_count"]= @invitee.unread_chat_count(params[:invitee_id])
+						data["created_at_with_event_timezone"] = @invitee.created_at_with_event_timezone 
+						data["updated_at_with_event_timezone"] = @invitee.updated_at_with_event_timezone
+						invitees << data
+					end	
 				end	
 				render :status => 200, :json => {:status => "Success",:invitees => invitees}
 				#render :status => 200, :json => {:status => "Success",:invitees => @invitees.as_json(:only =>[:id,:first_name,:last_name],:except => [:created_at, :updated_at], :methods => [:profile_picture,:unread_chat_count,:created_at_with_event_timezone, :updated_at_with_event_timezone])}
