a47c5003a90fd41656af150fb80944839a9639cd
uday <udmagdum@gmail.com>
2016-10-03 17:45:10 +0530

    replace msg

--- app/controllers/api/v1/invitee_chats_controller.rb
+++ app/controllers/api/v1/invitee_chats_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::InviteeChatsController < ApplicationController
 				#@send_chats_invitee_ids = @chats.pluck(:member_ids).uniq.map { |x| x.to_i }
 				#@invitee_list = @recieve_chats_invitee_ids + @send_chats_invitee_ids 
 				@invitees_list = @chats.pluck(:sender_id,:member_ids).flatten.map { |x| x.to_i}.uniq
-				@invitees = Invitee.where("id IN (?)",@invitee_list)
+				@invitees = Invitee.where("id IN (?)",@invitees_list)
 				invitees = []
 				@invitees.each do |invitee|
 					data = {}
@@ -26,7 +26,7 @@ class Api::V1::InviteeChatsController < ApplicationController
 				render :status => 200, :json => {:status => "Success",:invitees => invitees}
 				#render :status => 200, :json => {:status => "Success",:invitees => @invitees.as_json(:only =>[:id,:first_name,:last_name],:except => [:created_at, :updated_at], :methods => [:profile_picture,:unread_chat_count,:created_at_with_event_timezone, :updated_at_with_event_timezone])}
 			else
-				render :status=>200, :json=>{:status=>"Failure",:message=>"chat Not Found."}
+				render :status=>200, :json=>{:status=>"Failure",:message=>"Invitees Not Found."}
 			end	 
 		else
 			render :status=>200, :json=>{:status=>"Failure",:message=>"please provide required paramters."}
