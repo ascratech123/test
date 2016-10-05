a8ec2d93b20cf7e88842b05b9929ba0b6f1287fc
uday <udmagdum@gmail.com>
2016-10-05 11:43:04 +0530

    remove commented code

--- app/controllers/api/v1/invitee_chats_controller.rb
+++ app/controllers/api/v1/invitee_chats_controller.rb
@@ -6,12 +6,6 @@ class Api::V1::InviteeChatsController < ApplicationController
 			@event = Event.find(params[:event_id])
 			@chats = @event.chats.where("sender_id = ? or member_ids = ?",params[:invitee_id],params[:invitee_id]).order(created_at: :desc)
 			if @chats.present?
-				
-				#@recieve_chats_invitee_ids = @chats.pluck(:sender_id).uniq		
-				#@send_chats_invitee_ids = @chats.pluck(:member_ids).uniq.map { |x| x.to_i }
-				#@invitees_list = @recieve_chats_invitee_ids + @send_chats_invitee_ids
-				
-				#@invitees_list = @chats.pluck(:sender_id,:member_ids).flatten.map { |x| x.to_i}.uniq
 				@invitees_list = @chats.pluck(:sender_id,:member_ids).flatten.map { |x| x.to_i}.uniq
 				invitees = []
 				@invitees_list.each do |invitee|
@@ -30,7 +24,6 @@ class Api::V1::InviteeChatsController < ApplicationController
 					end	
 				end	
 				render :status => 200, :json => {:status => "Success",:invitees => invitees}
-				#render :status => 200, :json => {:status => "Success",:invitees => @invitees.as_json(:only =>[:id,:first_name,:last_name],:except => [:created_at, :updated_at], :methods => [:profile_picture,:unread_chat_count,:created_at_with_event_timezone, :updated_at_with_event_timezone])}
 			else
 				render :status=>200, :json=>{:status=>"Failure",:message=>"Invitees Not Found."}
 			end	 
