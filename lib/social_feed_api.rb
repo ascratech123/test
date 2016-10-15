79bd29c3e636dcf6990a9ff54d3b93727900a0f9
uday <udmagdum@gmail.com>
2016-10-15 18:57:18 +0530

    social feed changes

--- lib/social_feed_api.rb
+++ lib/social_feed_api.rb
@@ -3,12 +3,10 @@ require 'httparty'
 class SocialFeedApi
 
 	include HTTParty
-	require 'uri'
 	def self.get_facebook_posts(facebook_tags,date)
 		if date == nil
 			HTTParty.get("https://graph.facebook.com/#{facebook_tags}/posts?access_token=EAAEUA07UHnEBABbXziqZBHoQ5sBQBBWsE1u8WQlFrydxcB4FpxWFI5BRV786UwuTkLfVdAIYUF67ZBsEGJB2BZA3KVGkDIlKcxOA48rZA0AUszqvZCLnQbswMgyV2EhMx0wZCoNKs5kGEeApEcWcjaaaK5uVGpS2kZD&limit=4")
 		else	
-			date = URI.encode(date) 
 			HTTParty.get("https://graph.facebook.com/#{facebook_tags}/posts?access_token=EAAEUA07UHnEBABbXziqZBHoQ5sBQBBWsE1u8WQlFrydxcB4FpxWFI5BRV786UwuTkLfVdAIYUF67ZBsEGJB2BZA3KVGkDIlKcxOA48rZA0AUszqvZCLnQbswMgyV2EhMx0wZCoNKs5kGEeApEcWcjaaaK5uVGpS2kZD&limit=4&until=#{date}") 
 		end
 	end	
