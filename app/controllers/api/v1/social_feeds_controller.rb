072ac28967c6d915f2596eb876ab608df0da6799
uday <udmagdum@gmail.com>
2016-10-19 11:15:25 +0530

    social feed exclude retweet changes

--- app/controllers/api/v1/social_feeds_controller.rb
+++ app/controllers/api/v1/social_feeds_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SocialFeedsController < ApplicationController
 
 	def index
 	    @event = Event.find_by_id(params[:event_id]) 
-		if @event.present? or @event.facebook_social_tags.present? or @event.twitter_social_tags.present? 
+		if @event.present? or @event.facebook_social_tags.present? or @event.twitter_social_tags.present? #or @event.twitter_handle.present? 
 			#@facebook_posts
 			if @event.facebook_social_tags.present?
 				@facebook_posts = get_feacebook_posts(@event.facebook_social_tags)
@@ -13,7 +13,7 @@ class Api::V1::SocialFeedsController < ApplicationController
 				@facebook_posts = []
 			end
 		#twitter posts	
-		if @event.twitter_social_tags.present?
+		if @event.twitter_social_tags.present?# or @event.twitter_handle.present? 
 			@twitter_posts = get_twitter_posts(@event)
 		else
 			@twitter_posts = []
@@ -38,12 +38,17 @@ end
 	end	
 
 	def get_twitter_posts(event)
-		@twitter_tags =  event.twitter_social_tags.split(',').map{|x| x + " -rt"}.split(',').join(' +OR+ ')
+		#@twitter_tags =  @twitter_tags = event.twitter_social_tags.split(',').map{|x| '#'+x+ " -rt" }.join(' OR ') if event.twitter_social_tags.present?
+		@twitter_tags =  @twitter_tags = event.twitter_social_tags.split(',').map{|x| '#'+x}.join(' OR ') if event.twitter_social_tags.present?
 
+		#@twitter_tags =  event.twitter_social_tags.split(',').split(',').join(' OR ') if event.twitter_social_tags.present?
+		#@tweet_handle = event.twitter_handle if event.twitter_handle.present?
+		#@twitter_tags = @twitter_tags + " OR " +  @tweet_handle if event.twitter_social_tags.present? && event.twitter_handle.present? 
+		#@twitter_tags = event.twitter_handle if event.twitter_social_tags.blank? && event.twitter_handle.present? 
 		#@twitter_tags = twitter_social_tags.gsub(',','+OR+')	
 		session[:last_twitter_id] = nil if request.format == "html"
 		last_tweet_date = event.last_tweet_date.strftime("%Y-%m-%d") if event.last_tweet_date.present?
-		@twitter_posts = CLIENT.search(@twitter_tags, result_type: "recent",max_id:session[:last_twitter_id]).take(4)
+		@twitter_posts = CLIENT.search(@twitter_tags, result_type: "recent",since:"#{last_tweet_date}",max_id:session[:last_twitter_id],exclude:"retweets").take(4)
 		session[:last_twitter_id] = @twitter_posts.last.id-1  if @twitter_posts.present?
 		@twitter_posts = @twitter_posts.sort_by{ |k, v| k.created_at}.reverse!
 	  data = []
