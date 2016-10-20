4f176cecb02430ec2383cf8282f7007527ed639a
Gayatri <gayatri@ascratech.com>
2016-10-20 11:38:38 +0000

    changes on platform

--- app/models/invitee.rb
+++ app/models/invitee.rb
@@ -453,7 +453,7 @@ class Invitee < ActiveRecord::Base
   def get_all_mobile_app_users(mobile_app_code,submitted_app)
     event_ids = get_event_id(mobile_app_code,submitted_app)
     # invitees = Invitee.where("event_id IN (?) and  email = ?",event_ids, self.email) rescue nil
-    invitees = get_similar_invitees(event_ids).pluck(:id)
+    invitees = get_similar_invitees(event_ids)
     invitees = invitees.as_json(:only => [:first_name, :last_name,:designation,:id,:event_name,:name_of_the_invitee,:email,:company_name,:event_id,:about,:interested_topics,:country,:mobile_no,:website,:street,:locality,:location, :invitee_status, :provider, :linkedin_id, :google_id, :twitter_id, :facebook_id, :points, :created_at, :updated_at], :methods => [:qr_code_url,:profile_pic_url, :rank, :feedback_last_updated_at, :feedback_last_updated_at_with_event_timezone, :created_at_with_event_timezone, :updated_at_with_event_timezone]) if invitees.present?
     invitees
   end
@@ -526,15 +526,17 @@ class Invitee < ActiveRecord::Base
 
   def rank
     count = 0
-    invitees = Invitee.unscoped.where(:event_id => self.event_id, :visible_status => 'active').order('points desc') rescue nil
-    if invitees.present?
-      invitees.each do |inv|
-        count += 1
-        if inv.email == self.email
-          break
-        end
-      end
-    end
+    invitees = Invitee.unscoped.where(:event_id => self.event_id, :visible_status => 'active').order('points desc').pluck(:email) rescue nil
+    # if invitees.present?
+    #   invitees.each do |inv|
+    #     count += 1
+    #     if inv.email == self.email
+    #       break
+    #     end
+    #   end
+    # end
+    index = invitees.find_index(self.email)
+    count = index + 1 if index.present?
     count
   end
 
@@ -710,7 +712,7 @@ class Invitee < ActiveRecord::Base
 
   def self.get_read_notification(info, event_ids, user)
     # user_ids = Invitee.where("event_id IN (?) and  email = ?",event_ids, user.email).pluck(:id) rescue nil
-    user_ids = get_similar_invitees(event_ids).pluck(:id)
+    user_ids = user.get_similar_invitees(event_ids).pluck(:id) rescue nil
     data = []
     invitee_notifications = info.where(:invitee_id => user_ids) rescue nil
     data = invitee_notifications.as_json(:except => [:updated_at, :created_at]) if invitee_notifications.present?
@@ -721,7 +723,7 @@ class Invitee < ActiveRecord::Base
     start_event_date = start_event_date - 5.minutes
     info = InviteeNotification.where(:updated_at => start_event_date..end_event_date, event_id: event_ids)    
     # user_ids = Invitee.where("event_id IN (?) and  email = ?",event_ids, user.email).pluck(:id) rescue nil
-    user_ids = get_similar_invitees(event_ids).pluck(:id)
+    user_ids = user.get_similar_invitees(event_ids).pluck(:id) rescue nil
     invitee_notifications = info.where(:invitee_id => user_ids) rescue nil
     invitee_notifications.pluck(:notification_id) rescue []
   end
