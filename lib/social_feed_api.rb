76435fd0ef6591ee0bd0e5914575cd77331a9363
uday <udmagdum@gmail.com>
2016-10-21 11:49:57 +0530

    instagram get all user posts

--- lib/social_feed_api.rb
+++ lib/social_feed_api.rb
@@ -19,5 +19,11 @@ class SocialFeedApi
 
 	def self.get_all_instagram_posts(token,hash_tag)
 		instagram_data = HTTParty.get("https://api.instagram.com/v1/tags/#{hash_tag}/media/recent?&access_token=#{token}") rescue ""
+	end
+
+	def self.get_own_instgram_posts(event)
+		user_details = HTTParty.get("https://api.instagram.com/v1/users/self/?access_token=#{event.instagram_access_token}")
+		user_id = user_details["data"]["id"] if user_details.present?
+		instagram_data = HTTParty.get("https://api.instagram.com/v1/users/#{user_id}/media/recent/?access_token=#{event.instagram_access_token}") rescue ""
 	end	
 end
