bf58c74baaba8a3b35851439d0fafc91dc8a1a4c
uday <udmagdum@gmail.com>
2016-10-21 09:48:30 +0530

    sinagram changes

--- app/controllers/api/v1/social_feeds_controller.rb
+++ app/controllers/api/v1/social_feeds_controller.rb
@@ -26,19 +26,21 @@ class Api::V1::SocialFeedsController < ApplicationController
 	      	insta_acess_token = acess_token["access_token"]
 	      	@event.update_column('instagram_access_token',insta_acess_token)
 	    	end
-	      instagram_posts = SocialFeedApi.get_all_instagram_posts(@event.instagram_access_token,@event.instagram_social_tags) rescue []
-	        if instagram_posts["data"].present? and instagram_posts["data"] != "data"# and @event.facebook_social_tags.present? or  @event.twitter_social_tags.present?
-	      		session[:instagram_time_stamp] = (Date.today).to_datetime.to_i if request.format == "html"
-	          cuurent_date = Time.at(session[:instagram_time_stamp]) 
-	          date2 = (cuurent_date + 1.day).to_datetime.to_i if cuurent_date.present?
-	          data = instagram_posts["data"].select{ |k| k["created_time"].to_i.between?(session[:instagram_time_stamp],date2)}
+      	instagram_posts = SocialFeedApi.get_all_instagram_posts(@event.instagram_access_token,@event.instagram_social_tags) rescue []
+      	if @facebook_posts.blank? and @twitter_posts.blank? and request.format == "html"
+					@instgram_embedded_post = get_instagram_posts(instagram_posts["data"])
+				elsif instagram_posts["data"].present? and instagram_posts["data"] != "data" 
+      		session[:instagram_time_stamp] = (Date.today).to_datetime.to_i if request.format == "html"
+          cuurent_date = Time.at(session[:instagram_time_stamp]) 
+          date2 = (cuurent_date + 1.day).to_datetime.to_i if cuurent_date.present?
+          data = instagram_posts["data"].select{ |k| k["created_time"].to_i.between?(session[:instagram_time_stamp],date2)}
 
-	          @instgram_embedded_post = get_instagram_posts(data)    
-	        else
-	          @instgram_embedded_post = ""
-	        end
-	        previous_date = Time.at(session[:instagram_time_stamp]) if session[:instagram_time_stamp].present?
-	        session[:instagram_time_stamp] = (previous_date - 1.day).to_datetime.to_i if previous_date.present?
+          @instgram_embedded_post = get_instagram_posts(data)    
+        else
+          @instgram_embedded_post = ""
+        end
+        previous_date = Time.at(session[:instagram_time_stamp]) if session[:instagram_time_stamp].present?
+        session[:instagram_time_stamp] = (previous_date - 1.day).to_datetime.to_i if previous_date.present?
 	    end
 				if @instgram_embedded_post.present?
 					@total_posts = @facebook_posts + @twitter_posts + @instgram_embedded_post
@@ -48,22 +50,23 @@ class Api::V1::SocialFeedsController < ApplicationController
 		  	@social_feeds =  @total_posts.sort_by { |hsh| hsh[:created_at] }.reverse!
 		end	
   end
+
 end
 
 	def get_feacebook_posts(facebook_tags)
-			session[:facebook_post_date] = (Date.today+1).to_date if request.format == "html"
-			@posts = SocialFeedApi.get_facebook_posts(facebook_tags,session[:facebook_post_date])
-			if 	@posts.present? && @posts["data"].present?
-				session[:facebook_post_date] = @posts["data"].last["created_time"].to_date
-			else
-			  session[:facebook_post_date] = (session[:facebook_post_date]).to_date - 1		
-			end
-			data = []
-			@posts["data"].each do |post|
-				hsh = {:facebook_frames =>post["id"],:created_at=>post["created_time"].to_datetime}
-				data << hsh
-			end if @posts["data"].present?
-			@facebook_posts = data
+		session[:facebook_post_date] = (Date.today+1).to_date if request.format == "html"
+		@posts = SocialFeedApi.get_facebook_posts(facebook_tags,session[:facebook_post_date])
+		if 	@posts.present? && @posts["data"].present?
+			session[:facebook_post_date] = @posts["data"].last["created_time"].to_date
+		else
+		  session[:facebook_post_date] = (session[:facebook_post_date]).to_date - 1		
+		end
+		data = []
+		@posts["data"].each do |post|
+			hsh = {:facebook_frames =>post["id"],:created_at=>post["created_time"].to_datetime}
+			data << hsh
+		end if @posts["data"].present?
+		@facebook_posts = data
 	end	
 
 	def get_twitter_posts(event)
