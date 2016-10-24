f31308add14f7b497d2452a35cb1110c119d53f5
Gayatri <gayatri@ascratech.com>
2016-10-24 10:41:24 +0000

    rejected conversation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -23,7 +23,7 @@ class Conversation < ActiveRecord::Base
   validates :event_id, :user_id, presence: { :message => "This field is required." }
 
   after_create :set_status_as_per_auto_approve, :create_analytic_record, :set_event_timezone#, :set_dates_with_event_timezone
-  after_save :update_last_updated_model, :set_last_interaction_at
+  after_save :update_last_updated_model, :set_last_interaction_at, :update_analytics
 
   scope :desc_ordered, -> { order('updated_at DESC') }
   scope :asc_ordered, -> { order('updated_at ASC') }
