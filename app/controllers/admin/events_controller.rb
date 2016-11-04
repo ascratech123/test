4e94feaa21801e7bf4bd4e111f20e3754f251618
Sushil <sushil@ascratech.in>
2016-11-04 15:24:59 +0530

    default image create on dummy event issue solved

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -27,7 +27,7 @@ class Admin::EventsController < ApplicationController
 
   def new
     @event = @client.events.build
-    @event.images.build
+    @event.images.build unless (params[:marketing_app].present? and params[:marketing_app] == "true")
     @themes = Theme.find_themes()
     @default_features = @event.set_features_default_list
     @present_feature = @event.set_features rescue []
