543106447ef9fbc2ecf0948f62ca2aad45d5839c
Gayatri <gayatri@ascratech.com>
2016-11-15 12:39:31 +0000

    notification logger message changes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -141,7 +141,7 @@ class Notification < ActiveRecord::Base
   def push_to_ios(token, notification, push_pem_file, ios_obj, b_count, msg, push_page, type, time, title)
     notification = Grocer::Notification.new("device_token" => token, "alert"=>{"title"=> title, "body"=> msg, "action"=> "Read"}, 'content_available' => true, "badge" => b_count, "sound" => "siren.aiff", "custom" => {"push_page" => push_page, "id" => '1', 'event_id' => notification.event_id, 'image_url' => notification.image.url, 'type' => type, 'created_at' => time, 'notification_id' => notification.id, "actionable_id" => notification.actionable_id})
     response = ios_obj.push(notification) rescue nil
-    Rails.logger.info("******************************#{response}****************************************************")
+    Rails.logger.info("**IOS****************************#{response}****************************************************")
   end
 
   def self.get_action_based_invitees(invitees, notification_type)
