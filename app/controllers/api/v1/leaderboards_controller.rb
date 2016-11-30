1f73124673d34defb86151c205879221365a8a0e
Chirag Arya <chiragarya1992@gmail.com>
2016-11-30 17:28:44 +0530

    Mobile Application Code changes

--- app/controllers/api/v1/leaderboards_controller.rb
+++ app/controllers/api/v1/leaderboards_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::LeaderboardsController < ApplicationController
 
 	def index
 		# mobile_application = MobileApplication.find_by_submitted_code(params[:mobile_application_code]) || MobileApplication.find_by_preview_code(params[:mobile_application_preview_code]) 
-    if params[:mobile_application_preview_code].present? 
-      mobile_application = MobileApplication.find_by_preview_code(params[:mobile_application_preview_code])
-    elsif params[:mobile_application_code].present? or params["mobile_application_id"].present?
-      mobile_application = MobileApplication.where('submitted_code =? or preview_code =?', params[:mobile_application_code], params[:mobile_application_code]).first
-    end
+    # if params[:mobile_application_preview_code].present? 
+    #   mobile_application = MobileApplication.find_by_preview_code(params[:mobile_application_preview_code])
+    # if params[:mobile_application_code].present? or params["mobile_application_id"].present?
+    mobile_application = MobileApplication.where('submitted_code =? or preview_code =?', params[:mobile_application_code], params[:mobile_application_code]).first if params[:mobile_application_code].present? or params["mobile_application_id"].present?
+    # end
 
 		# event_status = (params[:mobile_application_code].present? ? ["published"] : ["approved","published"])
 
