8f0370c9c2ce70584d8e017b9682c53f801dfcc9
Chirag Arya <chiragarya1992@gmail.com>
2016-12-02 12:28:31 +0530

    Syntax Error Fix

--- app/controllers/api/v1/mobile_applications_controller.rb
+++ app/controllers/api/v1/mobile_applications_controller.rb
@@ -7,14 +7,14 @@ class Api::V1::MobileApplicationsController < ApplicationController
   def show
     if params[:mobile_application_preview_code].present? 
       @mobile_application = MobileApplication.find_by_preview_code(params[:mobile_application_preview_code])
-    elsif params[:mobile_application_code].present?
-      @mobile_application = MobileApplication.where('submitted_code =? or preview_code =?', params[:mobile_application_code], params[:mobile_application_code]).first
+    elsif params[:mobile_application_code].present? or params["mobile_application_id"].present?
+      @mobile_application = MobileApplication.where('submitted_code =? or preview_code =? or id =?', params[:mobile_application_code], params[:mobile_application_code], params["mobile_application_id"]).first
     elsif params["id"].present?
       @mobile_application = MobileApplication.where('submitted_code =? or preview_code =?', params[:id], params[:id]).first
     end
     if @mobile_application.present?
       create_device_token
-      render :status => 200, :json => {:status => "Success", :mobile_application => @mobile_application.as_json(:only => [:id,:login_background_color,:message_above_login_page,:registration_message, :registration_link, :login_button_color, :login_button_text_color, :listing_screen_text_color, :social_media_status], :methods => [:app_icon_url, :splash_screen_url, :login_background_url, :listing_screen_background_url ]) }
+      render :status => 200, :json => {:status => "Success", :mobile_application => @mobile_application.as_json(:only => [:id,:login_background_color,:message_above_login_page,:registration_message, :registration_link, :login_button_color, :login_button_text_color, :listing_screen_text_color, :social_media_status, :visitor_registration,:social_media_logins, :choose_home_page, :home_page_event_id, :app_icon_updated_at,:splash_screen_updated_at,:login_background_updated_at,:listing_screen_background_updated_at], :methods => [:app_icon_url, :splash_screen_url, :login_background_url, :listing_screen_background_url, :visitor_registration_background_image_url, :visitor_registration_back_color, :marketing_app_event_id ]) }   
     else
       render :status => 200, :json => {:status => "Failure", :message => "Mobile Application Not Found."}
     end
