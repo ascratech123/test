39a1c4a206b1b772eaa4d9c9f2d92222b9946e1a
Sushil <sushil@ascratech.in>
2016-12-14 12:47:22 +0530

    delete api changes

--- lib/sync_mobile_data.rb
+++ lib/sync_mobile_data.rb
@@ -42,7 +42,9 @@ module SyncMobileData
         deleted_value = model_name.constantize.find_by_id(value["id"])
         if deleted_value.destroy
           #message << deleted_value.id
-          message << {:action => "destroy","resourse_id" => deleted_value.id,"resourse_type" => deleted_value.favoritable_type}
+          resourse_type = deleted_value.favoritable_type if model_name == "Favorite"
+          resourse_type = deleted_value.likable_type if model_name == "Like"
+          message << {:action => "destroy","resourse_id" => deleted_value.id,"resourse_type" => resourse_type}
         end if deleted_value.present?
       end
     end  
