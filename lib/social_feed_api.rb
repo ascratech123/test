a837e29a3125ef90c52ad72a3d380cde017d989b
uday <udmagdum@gmail.com>
2016-12-23 13:46:28 +0530

    added new acess token

--- lib/social_feed_api.rb
+++ lib/social_feed_api.rb
@@ -8,8 +8,8 @@ class SocialFeedApi
 		# 	facebook_data = HTTParty.get("https://graph.facebook.com/#{facebook_tags}/posts?access_token=EAAEUA07UHnEBABbXziqZBHoQ5sBQBBWsE1u8WQlFrydxcB4FpxWFI5BRV786UwuTkLfVdAIYUF67ZBsEGJB2BZA3KVGkDIlKcxOA48rZA0AUszqvZCLnQbswMgyV2EhMx0wZCoNKs5kGEeApEcWcjaaaK5uVGpS2kZD&limit=4") rescue ""
 		# else	
 		# 	facebook_data = HTTParty.get("https://graph.facebook.com/#{facebook_tags}/posts?access_token=EAAEUA07UHnEBABbXziqZBHoQ5sBQBBWsE1u8WQlFrydxcB4FpxWFI5BRV786UwuTkLfVdAIYUF67ZBsEGJB2BZA3KVGkDIlKcxOA48rZA0AUszqvZCLnQbswMgyV2EhMx0wZCoNKs5kGEeApEcWcjaaaK5uVGpS2kZD&limit=4&until=#{date}") rescue ""
-		# end	
-			facebook_data = HTTParty.get("https://graph.facebook.com/#{facebook_tags}/posts?access_token=EAAEUA07UHnEBABbXziqZBHoQ5sBQBBWsE1u8WQlFrydxcB4FpxWFI5BRV786UwuTkLfVdAIYUF67ZBsEGJB2BZA3KVGkDIlKcxOA48rZA0AUszqvZCLnQbswMgyV2EhMx0wZCoNKs5kGEeApEcWcjaaaK5uVGpS2kZD&limit=20") rescue ""
+		# end
+			facebook_data = HTTParty.get("https://graph.facebook.com/#{facebook_tags}/posts?access_token=EAACEdEose0cBADLHmNiZCnHazZBvX1pTi31msLtMBEZCYJsJIuwlJJFBHnr4HCRyboUVoicZCukZB6Ezbf47q90b3DZBFtWeZBiYozuZCqJRzJdHZBSsUOFtgqZB8EAMfkp1xWrfpaZC3Xb46rQtIcbpKplQ7rDyPrGiuuOzNENI11qZCgZDZD&limit=20") rescue ""
 	end	
 
 	def self.get_twitter_posts(url)
